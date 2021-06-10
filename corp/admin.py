6bacd173ec59677fccc16e747ff91a90274e9f97
Gurvan Duault <gurvan.duault@etudiant.univ-rennes1.fr>
2021-06-10 16:25:57 +0200

    Ajout des models dans le catalogue

--- corp/admin.py
+++ corp/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-
 # Register your models here.
 
-from django.contrib import admin
-from .models import Adherent
+from corp.models import Adherent, Certif_Medical, Assurance_Complementaire, Evenement, Type_Evenement
+
+admin.site.register(Adherent)
+admin.site.register(Certif_Medical)
+admin.site.register(Assurance_Complementaire)
+admin.site.register(Evenement)
+admin.site.register(Type_Evenement)
+
+
