8e9585487c935374fb39941e96f6a3b7ac3421ed
Gurvan Duault <gurvan.duault@etudiant.univ-rennes1.fr>
2021-06-10 13:50:01 +0200

    Ajout des fichier

--- corp/views.py
+++ corp/views.py
@@ -3,7 +3,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
-
+from CSRF import token
 
 # Create your views here.
 
