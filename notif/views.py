8e15601d6d5a35b440f58438e6636fd774b33e73
Aquillina <achassanledantec@gmail.com>
2021-06-28 10:43:07 +0200

    notif V2

--- notif/views.py
+++ notif/views.py
@@ -23,7 +23,7 @@ def notification(request):
 
 
 
-    # Variable avec la date d'inscription de la licence +1 ans
+    # Variable avec la date d'inscription de la licence +1 ans (pour avoir la date de fin de la licence)
     NLicence = Dlicence + datetime.timedelta(days=365)
 
 
@@ -36,7 +36,7 @@ def notification(request):
     Dcertif = "2021/05/18"                    # à remplacer par la donnée correspondante dans la bdd
     Dcertif = datetime.datetime.strptime(Dcertif, "%Y/%m/%d")
     # variable avec la fin de la certif medical avec le format année/mois/jours
-    Fcertif = "2021/06/27"                                  # à remplacer par la donnée correspondante dans la bdd
+    Fcertif = "2021/06/30"                                  # à remplacer par la donnée correspondante dans la bdd
     Fcertif = datetime.datetime.strptime(Fcertif, "%Y/%m/%d")
 
     # je met dans alerteCertif la date à partir de quand la notif doit apparaitre
@@ -52,6 +52,7 @@ def notification(request):
     compt1 = 0
     compt2 = 0
     compt3 = 0
+    compt4 = 1
 
     #Variable pour ne plus afficher les notifications
     RenouLice = 0
@@ -63,22 +64,27 @@ def notification(request):
     NotiflicenceTresorier = 1  # à lier a un bouton sur la page gestion de membre du tresorier
 
     # condition d'activation des notifications et compteur de notification
+
+        # si le bouton du tresorier dans gestion membre/licence est activer, envoye une notification
     if NotiflicenceTresorier == 1:
         NB_NOTIF = NB_NOTIF + 1
         compt = 1
-
+        # si le bouton du tresorier dans gestion membre/assurance est activer, envoye une notification
     if NotifassuranceTresorier == 1:
         NB_NOTIF = NB_NOTIF + 1
-        compt3 = 4
-
+        compt3 = 1
+        #si la date du jour est superieur a la date d'alerte licence ( 1ans apres la date d'inscription licence moins 1 mois) envoye une notif
     if today > alerteLicence:
         NB_NOTIF = NB_NOTIF + 1
-        compt1 = 2
-
+        compt1 = 1
+        #si la date du jour est superieur a la date d'alerte de la certif ( fin de certif moins 2 semaine ) envoye une certif
+        # et si la date du jour est inférieur a la date de fin de certif
     if today > alerteCertif:
         if today < Fcertif:
             NB_NOTIF = NB_NOTIF + 1
-            compt2 = 3
+            compt2 = 1
+
+       # si un des champs Nom, prenom, lieu de naissance, date de  naissance, tel. portable, Email, adresse 1, code postal, ville, pays, personne a contacter, num personne a contacter, medecin reférent ou num tel medecin referent est vide envoye une notif pour dire de remplir les information dans gestion de profile
 
 
 
@@ -89,9 +95,10 @@ def notification(request):
         "compt1": compt1,
         "compt2": compt2,
         "compt3": compt3,
+        "compt4": compt4,
         "NotiflicenceTresorier": NotiflicenceTresorier,
         "NotifassuranceTresorier": NotifassuranceTresorier,
-        "RenouLice" : RenouLice,
+        "RenouLice": RenouLice,
         "RenouCertif": RenouCertif,
     }
 
